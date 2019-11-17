30500aa7d200ca57814dcdd8feb2800222326743
Szymon Koper <sz.koper@gmail.com>
2019-11-17 21:40:36 +0100

    Adds fake data

---
+++ fake/highscores.js
@@ -0,0 +1,34 @@
+const minesweeper = {
+  minesweeper: [
+    {
+      id: '1',
+      name: "szymcio",
+      datetime: "2019-11-17T19:55:44+00:00",
+      score: {
+        duration: 50.13,
+        cellsCount: 100
+      }
+    },
+
+    {
+      id: '2',
+      name: "szymcio",
+      datetime: "2019-11-17T19:55:44+00:00",
+      score: {
+        duration: 66.568,
+        cellsCount: 100
+      }
+    },
+    {
+      id: '3',
+      name: "janusz",
+      datetime: "2019-11-17T19:55:44+00:00",
+      score: {
+        duration: 58.09,
+        cellsCount: 64
+      }
+    }
+  ]
+}
+
+module.exports = minesweeper
