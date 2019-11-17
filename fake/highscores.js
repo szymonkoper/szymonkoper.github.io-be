72f19561cd6e93a18faa4fdf8b7c5fc99ed90382
Szymon Koper <sz.koper@gmail.com>
2019-11-17 22:33:31 +0100

    Adds post endpoint without persistence

--- fake/highscores.js
+++ fake/highscores.js
@@ -1,30 +1,33 @@
 const minesweeper = {
   minesweeper: [
     {
-      id: '1',
-      name: "szymcio",
+      id: "1",
+      username: "szymcio",
       datetime: "2019-11-17T19:55:44+00:00",
       score: {
         duration: 50.13,
+        minesCount: 10,
         cellsCount: 100
       }
     },
 
     {
-      id: '2',
-      name: "szymcio",
+      id: "2",
+      username: "szymcio",
       datetime: "2019-11-17T19:55:44+00:00",
       score: {
         duration: 66.568,
+        minesCount: 10,
         cellsCount: 100
       }
     },
     {
-      id: '3',
-      name: "janusz",
+      id: "3",
+      username: "janusz",
       datetime: "2019-11-17T19:55:44+00:00",
       score: {
         duration: 58.09,
+        minesCount: 8,
         cellsCount: 64
       }
     }
--- routes/api/highscores.js
+++ routes/api/highscores.js
@@ -1,40 +1,77 @@
 const express = require('express')
+const uuid = require('uuid')
+const dateFns = require('date-fns')
+const { parseIso, format } = dateFns
+// TODO: Add ES6 support (imports)
 const router = express.Router()
 const highscores = require('../../fake/highscores')
 
-const createErrorForGameNotFound = game => ({ message: `No game found: ${game}` })
-const createErrorForGameScoreNotFound = game => ({
-  message: `No highscore found with id: ${game}`
+const DATETIME_FORMAT = "yyyy-LL-dd'T'HH:mm:ssxx"
+const formatDate = date => format(date, DATETIME_FORMAT)
+const parseDate = dateString => parse(DATETIME_FORMAT, dateString)
+
+// RESPONSES
+const createResponse = (message, data) => ({ message, data })
+
+// RESPONSES success
+const createResponseForFoundScores = scores => createResponse('Game scores found', scores)
+
+const createResponseForFoundScore = score => createResponse('Game score found', score)
+
+// RESPONSES console.error(
+const createErrorForScoresNotFound = (gameId) =>
+  createResponse(
+    `No score found for gameId: ${gameId}`
+  )
+
+const createErrorForScoreNotFound = (gameId, scoreId) =>
+  createResponse(
+    `No score found with gameId: ${gameId}, scoreId: ${scoreId}`
+  )
+
+// SELECTORS
+const scoresSelector = gameId => highscores[gameId]
+
+const scoreSelector = (gameId, scoreId) => (scoresSelector(gameId) || []).find(({ id }) => id === scoreId)
+
+// ROUTES
+router.get('/:gameId', (req, res) => {
+  const { gameId } = req.params
+  const foundScores = scoresSelector(gameId)
+
+  if (!foundScores) {
+    return res.status(404).json(createErrorForScoresNotFound(gameId))
+  }
+
+  return res.json(createResponseForFoundScores(foundScores))
 })
 
-const gameSelector = (game) => highscores[game]
+router.get('/:gameId/:scoreId', (req, res) => {
+  const { gameId, scoreId } = req.params
+  const foundScore = scoreSelector(gameId, scoreId)
 
-router.get('/:game', (req, res) => {
-  const { game } = req.params
-  const foundGame = gameSelector(game)
-
-  if (!foundGame) {
-    return res.status(404).json(createErrorForGameNotFound(game))
+  if (!foundScore) {
+    return res.status(404).json(createErrorForScoreNotFound(gameId, scoreId))
   }
 
-  return res.json(foundGame)
+  return res.json(createResponseForFoundScore(foundScore))
 })
 
-router.get("/:game/:id", (req, res) => {
-  const { game, id } = req.params
-  const foundGame = gameSelector(game)
+router.post('/:gameId', (req, res) => {
+  const { gameId } = req.params
+  const { username, score } = req.body
 
-  if (!foundGame) {
-    return res.status(404).json(createErrorForGameNotFound(game))
+  const newScore = {
+    id: uuid.v4(),
+    username,
+    datetime: formatDate(new Date()),
+    score
   }
 
-  const foundHighscores = foundGame.find(highscore => highscore.id === id)
 
-  if (!foundHighscores) {
-    return res.status(404).json(createErrorForGameScoreNotFound(id))
-  }
+  const foundScores = scoresSelector(gameId)
 
-  return res.json(foundHighscores)
+  return res.json([...foundScores, newScore])
 })
 
 module.exports = router
